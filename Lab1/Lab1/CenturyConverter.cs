7da9fbfa9ff5fd30c0be3d4d0a2e645362332a86
Schepin <chznz@mail.ru>
2018-02-13 10:28:27 +0600

    Add Lab1
    Add Lab1

---
+++ Lab1/Lab1/CenturyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab1
+{
+    class CenturyConverter
+    {
+        public string ConvertYear(string st)
+        {
+
+            int year = Convert.ToInt32(st);
+
+            year = (year - 1) / 100;
+            year = year + 1;
+
+            string century = year.ToString();
+            return century;
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Дан номер некоторого года (положительное целое число). Вывести
+соответствующий ему номер столетия, учитывая, что, к примеру,
+началом 20 столетия был 1901 год.*/
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CenturyConverter TranslationFromYearToCentury = new CenturyConverter();
+            Console.WriteLine("Введите год");
+            string Century = Console.ReadLine();
+            string cent = TranslationFromYearToCentury.ConvertYear(Century);
+            Console.WriteLine("Результат: {0} век", cent);
+            Console.ReadLine();
+        }
+    }
+}
