00d8624f99f45f22e5c35541947ab306693a1dbe
Schepin <chznz@mail.ru>
2018-02-20 09:55:36 +0600

    Lab2 Finish
    Lab2 Finish

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Дана строка, содержащая несколько круглых скобок. Если скобки
+расставлены правильно (то есть каждой открывающей соответствует
+одна закрывающая), то вывести число 0. В противном случае
+вывести или номер позиции, в которой расположена первая
+ошибочная закрывающая скобка, или, если закрывающих скобок не
+хватает, число –1.*/
+
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TextAnalysis CountingBracketsInText = new TextAnalysis();
+            Console.WriteLine("Введите текст");
+            string Text = Console.ReadLine();
+            int CountedAnswer = CountingBracketsInText.ScoringCount(Text);
+           
+            Console.WriteLine("Результат: {0} ", CountedAnswer);
+            if (CountedAnswer == 0) Console.WriteLine("Скобки расставлены правильно");
+            if (CountedAnswer == -1) Console.WriteLine("Не хватает закрывающей скобки");
+            if (CountedAnswer > 0) Console.WriteLine("Ответ ({0}) равен номеру позиции, в которой расположена первая ошибочная закрывающая скобка", CountedAnswer);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab2/Lab2/TextAnalysis.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab2
+{
+    public class TextAnalysis
+    {
+        public int ScoringCount(string text)
+        {
+            int Analysis = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ')')
+                {
+                    Analysis++;
+                }
+                if (text[i] == '(')
+                {
+                    Analysis--;
+                }
+                if (Analysis > 0)
+                {
+                    Analysis = i + 1;
+                    break;
+                }
+            }
+            return Analysis;
+        }
+    }
+}
